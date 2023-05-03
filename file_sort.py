545e6e4120c65abb07707f04e02cd7f7cf390fef
hmtyj2 <ghkdalsxkr@gmail.com>
2023-05-03 12:57:27 +0900

    revised

--- file_sort.py
+++ file_sort.py
@@ -5,5 +5,3 @@ for lower_case in ['a', 'b', 'c']:
     dest_dir = '../{}'.format(lower_case.upper())
     file_name = '{}.txt'.format(lower_case)
     shutil.move('{}/{}'.format(target_dir, file_name), '{}/{}'.format(dest_dir, file_name))
-
-return
